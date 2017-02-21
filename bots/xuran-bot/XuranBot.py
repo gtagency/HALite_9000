4d862cdad1aa58fd4047124fc17e584aa639f631
xuranchen <xuran.chen@gmail.com>
2017-02-21 18:15:02 -0500

    fix

--- bots/xuran-bot/XuranBot.py
+++ bots/xuran-bot/XuranBot.py
@@ -2,22 +2,65 @@ import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
 
-
 myID, game_map = hlt.get_init()
-hlt.send_init("XuranBot")
+moveCounter = 0
 
-def assign_move(square):
-    for direction, neighbor in enumerate(game_map.neighbors(square)):
-        if neighbor.owner != myID and neighbor.strength < square.strength:
-            return Move(square, direction)
+def run_at_start():
+    values = []
+    for n in game_map:
+        val = 0
+        for o in game_map.neighbors(n, 2, True):
+            val += o.strength
+        values.append((n, val))
+    values.sort(key=lambda tup: tup[1], reverse=True)
+    values = values[:(game_map.width//10)]
+    hlt.send_init("XuranBot")
 
-    if square.strength < 5 * square.production:
-        return Move(square, STILL)
+
+def find_nearest_enemy_direction(square):
+    direction = NORTH
+    max_distance = min(game_map.width, game_map.height) / 2
+    for d in (NORTH, EAST, SOUTH, WEST):
+        distance = 0
+        current = square
+        while current.owner == myID and distance < max_distance:
+            distance += 1
+            current = game_map.get_target(current, d)
+        if distance < max_distance:
+            direction = d
+            max_distance = distance
+    return direction
+
+
+def heuristic(square):
+    if square.owner == 0 and square.strength > 0:
+        return square.production / square.strength
     else:
-        return Move(square, random.choice((NORTH, WEST)))
+        # return total potential damage caused by overkill when attacking this square
+        return sum(neighbor.strength for neighbor in game_map.neighbors(square) if neighbor.owner not in (0, myID))
 
 
+def get_move(square):
+    target, direction = max(((neighbor, direction) for direction, neighbor in enumerate(game_map.neighbors(square))
+                             if neighbor.owner != myID),
+                            default=(None, None),
+                            key=lambda t: heuristic(t[0]))
+    if target is not None and target.strength < square.strength:
+        return Move(square, direction)
+    elif square.strength < square.production * 5:
+        return Move(square, STILL)
+
+    border = any(neighbor.owner != myID for neighbor in game_map.neighbors(square))
+    if not border:
+        return Move(square, find_nearest_enemy_direction(square))
+    else:
+        # wait until we are strong enough to attack
+        return Move(square, STILL)
+
+run_at_start()
 while True:
     game_map.get_frame()
-    moves = [assign_move(square) for square in game_map if square.owner == myID]
-hlt.send_frame(moves)
+    moves = [get_move(square) for square in game_map if square.owner == myID]
+    moveCounter += 1
+    hlt.send_frame(moves)
+
